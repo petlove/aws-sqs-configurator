569edd5e75f1615ca1dc5d8ac4afd4df565f5ab8
Lincoln Rodrigues <lincolnrodrs@gmail.com>
2019-07-18 21:14:49 -0300

    remove logs (#8)

--- lib/aws/sqs/configurator/logger.rb
+++ lib/aws/sqs/configurator/logger.rb
@@ -4,13 +4,15 @@ module AWS
   module SQS
     module Configurator
       module Logger
+        LOGGER_ENABLED_ENV = 'AWS_SQS_CONFIGURATOR_LOGGER'.freeze
+
         class << self
           def info(message)
-            puts log_info(message)
+            puts log_info(message) if log?
           end
 
           def error(message)
-            puts log_error(message)
+            puts log_error(message) if log?
           end
 
           def log_info(message)
@@ -22,6 +24,10 @@ module AWS
           end
 
           private
+
+          def log?
+            ENV[LOGGER_ENABLED_ENV]
+          end
 
           def log(severity_level, message)
             "[#{Time.now.iso8601}] [AWS::SQS::Configurator] #{severity_level} -- : #{message}"
